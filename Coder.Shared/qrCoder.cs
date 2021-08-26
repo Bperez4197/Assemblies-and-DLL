aaa6ceb5ebbbdedd37d458fc3422439d6abf334c
bryce <bryceperez31@gmail.com>
2021-08-26 14:00:17 -0700

    Add project files.

---
+++ Coder.Shared/qrCoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QRCoder;
+using System.Drawing;
+
+namespace Coder.Shared
+{
+    public class qrCoder
+    {
+
+        public void GenerateCode(string codeContent)
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            QRCodeData qrCodeData = generator.CreateQrCode(codeContent, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save("Address.bmp");
+        }
+        
+
+    }
+}
---
+++ Lesson3Addresses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coder.Shared;
+
+namespace Lesson3Addresses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the address: ");
+            string userAddy = Console.ReadLine();
+
+            qrCoder companyAddressCoder = new qrCoder();
+            companyAddressCoder.GenerateCode(userAddy);
+
+        }
+    }
+}
